8bbd77b2dc4d362acb76ea2541aa572d84c70719
Nahuel Grosso <nahuelgrosso1982@gmail.com>
2023-09-17 22:33:39 -0300

    Agregue colores segun la prioridad, primer commit desde el clon

--- app/tareas.php
+++ app/tareas.php
@@ -28,15 +28,71 @@ function verTareas()
                                     <b><?php echo $tarea->tarea; ?></b>
                                 </a>
                                 <!-- FIN POR MI CUENTA -->
+                                <?php
 
-                                <span class="badge bg-primary rounded-pill position-absolute top-0 end-0">Prioridad <?php echo $tarea->prioridad; ?></span>
+                                //si prioridad es == 1 => verde     text-bg-secondary
+                                //si es           == 2 => celeste   text-bg-info
+                                //si es           == 3 => azul      text-bg-primary
+                                //si es           == 4 => amarillo  text-bg-warning
+                                //si es           == 5 => rojo      text-bg-danger
+
+                                switch ($tarea->prioridad) {
+                                    case $tarea->prioridad == 1:
+                                        echo "bg-secondary";
+                                        break;
+                                    case $tarea->prioridad == 2:
+                                        echo "bg-info";
+                                        break;
+                                    case $tarea->prioridad == 3:
+                                        echo "bg-primary";
+                                        break;
+                                    case $tarea->prioridad == 4:
+                                        echo "bg-warning";
+                                        break;
+                                    case $tarea->prioridad == 5:
+                                        echo "bg-danger";
+                                        break;
+                                }
+                                ?>
+
+                                <span class="badge <?php
+
+                                                    //si prioridad es == 1 => verde     text-bg-secondary
+                                                    //si es           == 2 => celeste   text-bg-info
+                                                    //si es           == 3 => azul      text-bg-primary
+                                                    //si es           == 4 => amarillo  text-bg-warning
+                                                    //si es           == 5 => rojo      text-bg-danger
+
+                                                    switch ($tarea->prioridad) {
+                                                        case $tarea->prioridad == 1:
+                                                            echo "bg-secondary";
+                                                            break;
+                                                        case $tarea->prioridad == 2:
+                                                            echo "bg-info";
+                                                            break;
+                                                        case $tarea->prioridad == 3:
+                                                            echo "bg-primary";
+                                                            break;
+                                                        case $tarea->prioridad == 4:
+                                                            echo "bg-warning";
+                                                            break;
+                                                        case $tarea->prioridad == 5:
+                                                            echo "bg-danger";
+                                                            break;
+                                                    }
+                                                    ?> rounded-pill position-absolute top-0 end-0">Prioridad <?php echo $tarea->prioridad; ?></span>
                                 <div class=" ">
                                     <a href="eliminar/<?php echo $tarea->id ?>">
-                                        <button type="button" class="btn btn-danger">Eliminar</button>
+                                        <button type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                            </svg> Eliminar</button>
                                     </a>
                                     <?php if (!$tarea->hecha) { ?>
                                         <a href="realizada/<?php echo $tarea->id ?>">
-                                            <button type="button" class="btn btn-success">Realizada</button>
+                                            <button type="button" class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                                </svg> Realizada</button>
                                         </a>
                                     <?php } ?>
                                 </div>
@@ -69,11 +125,16 @@ function verTareas()
 
 
                         <a href="eliminar/<?php echo $tarea->id ?>">
-                            <button type="button" class="btn btn-danger">Eliminar</button>
+                            <button type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                </svg> Eliminar</button>
                         </a>
                         <?php if (!$tarea->hecha) { ?>
                             <a href="realizada/<?php echo $tarea->id ?>">
-                                <button type="button" class="btn btn-success">Realizada</button>
+                                <button type="button" class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                    </svg> Realizada</button>
                             </a>
                         <?php } ?>
                     </div>
